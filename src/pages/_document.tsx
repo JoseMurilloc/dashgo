59910de81f01ae749dc53e687696b4a4a51ebec5
Jose Murillo <jooseemurillo@gmail.com>
2021-03-31 21:51:15 -0300

    ✨ Initial commit and Config

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
