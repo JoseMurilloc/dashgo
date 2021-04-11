adb160115b4d767f015541bde716751c66995188
Jose Murillo <jooseemurillo@gmail.com>
2021-04-11 09:07:31 -0300

    🚀 Config react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "react-query"
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext"
 import { makeServer } from "../services/mirage"
 import { theme } from "../styles/theme"
@@ -8,13 +9,17 @@ if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SidebarDrawerProvider>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </SidebarDrawerProvider>
+    <QueryClientProvider client={queryClient}>
+      <SidebarDrawerProvider>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </SidebarDrawerProvider>
+    </QueryClientProvider>
   )
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,8 +10,18 @@ import { Header } from '../../components/Header';
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from '../../components/Sidebar';
 import Link from 'next/link';
+import { useQuery } from "react-query";
+import { Spinner } from "@chakra-ui/spinner";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch(`http://localhost:3000/api/users`)
+    const data = await response.json()
+
+    return data
+  })
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -47,54 +57,62 @@ export default function UserList() {
             </Link>
           </Flex>
         
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink"/>
-                </Th>
-                <Th>Usuários</Th>
-                { isWideVersion && <Th>Data de cadastro</Th>}
-                { isWideVersion && <Th>Ações</Th>}
-              </Tr>
-            </Thead>
-            <Tbody>
-            <Tr px={["4", "4", "6"]}>
-                <Td>
-                  <Checkbox colorScheme="pink"/>
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">José Murillo</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      jooseemurillo@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && (
-                  <Td>
-                    04 de Abril de 2021
-                  </Td>
-                )}
-                {isWideVersion && (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      colorScheme="purple"
-                      leftIcon={<Icon as={RiPencilLine} />}
-                      cursor="pointer"
-                    >
-                      Editar
-                    </Button>
-                  </Td>  
-                )}
-              </Tr>
-               
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Spinner />
+          ) : error ? (
+            <Text>Error ao carregar os dados do usúario</Text>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px="6" color="gray.300" width="8">
+                      <Checkbox colorScheme="pink"/>
+                    </Th>
+                    <Th>Usuários</Th>
+                    { isWideVersion && <Th>Data de cadastro</Th>}
+                    { isWideVersion && <Th>Ações</Th>}
+                  </Tr>
+                </Thead>
+                <Tbody>
+                <Tr px={["4", "4", "6"]}>
+                    <Td>
+                      <Checkbox colorScheme="pink"/>
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">José Murillo</Text>
+                        <Text fontSize="sm" color="gray.300">
+                          jooseemurillo@gmail.com
+                        </Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && (
+                      <Td>
+                        04 de Abril de 2021
+                      </Td>
+                    )}
+                    {isWideVersion && (
+                      <Td>
+                        <Button
+                          as="a"
+                          size="sm"
+                          colorScheme="purple"
+                          leftIcon={<Icon as={RiPencilLine} />}
+                          cursor="pointer"
+                        >
+                          Editar
+                        </Button>
+                      </Td>  
+                    )}
+                  </Tr>
+                  
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
